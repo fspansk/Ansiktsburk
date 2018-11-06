506193c11ad45657d30f8e8806c9e13580c0a1f8
fspansk <fredrik.spansk@gmail.com>
2018-11-06 10:44:47 +0100

    Add project files.

---
+++ Ansiktsburk/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Ansiktsburk.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
